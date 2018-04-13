50eadec7d16c27f65647dc0ea2a2ec26412dcb6f
FHGDev <gideon.lingle14@gmail.com>
2018-04-13 12:43:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,3 +6,15 @@ bot.on('ready', () => {
   bot.user.setActivity(`Fallout 4`, {type: "PLAYING"})
   console.log("Bot ready.")
 })
+
+bot.on('message', message => {
+  let args = message.content.split(" ").slice(1)
+  if (message.author.bot) return;
+  if (message.author.id == "369313004539346945") {
+    if (message.content.startsWith("!changestatus")) {
+      bot.user.setActivity(args.join(' '), {type: "PLAYING"})
+    }
+  } else {
+    return;
+  }
+})
