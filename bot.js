4925009b2a725c723bff51012f10f007273da6e3
FHGDev <gideon.lingle14@gmail.com>
2018-04-13 13:03:41 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const discord = require('discord.js')
 const bot = new discord.Client()
 const idarray = ["369313004539346945", "242734840829575169"]
 bot.login(process.env.rtoken)
+const prefix = "!"
 
 bot.on('ready', () => {
   bot.user.setActivity(`Fallout 4`, {type: "PLAYING"})
@@ -9,11 +10,12 @@ bot.on('ready', () => {
 })
 
 bot.on('message', message => {
-  let args = message.content.split(" ").slice(1)
+  let mArray = message.content.split(" ");
+  let args = mArray.slice(1)
   if (message.author.bot) return;
   if (idarray.includes(message.author.id)) {
     if (message.content.startsWith("!changestatus")) {
-      console.log("REJEN used a cmd.")
+      console.log("REJEN and/or Hulk used a cmd.")
       bot.user.setActivity(args.join(' '), {type: "PLAYING"})
     }
   } else {
