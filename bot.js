33b43ccd43cd9de4ac419e4b840d77372e06f4fe
FHGDev <gideon.lingle14@gmail.com>
2018-04-13 12:56:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,10 +8,11 @@ bot.on('ready', () => {
 })
 
 bot.on('message', message => {
-  let args = message.content.split(" ").slice(0)
+  let args = message.content.split(" ").slice(1)
   if (message.author.bot) return;
   if (message.author.id == "369313004539346945") {
     if (message.content.startsWith("!changestatus")) {
+      console.log("REJEN used a cmd.")
       bot.user.setActivity(args.join(' '), {type: "PLAYING"})
     }
   } else {
