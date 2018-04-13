12de91d9b9ce8dc58106abc261b1d521773f56b6
FHGDev <gideon.lingle14@gmail.com>
2018-04-13 12:31:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const discord = require('discord.js')
 const bot = new discord.Client()
-bot.login(process.env.username, process.env.password)
+bot.login(process.env.rtoken)
 
 bot.on('ready', () => {
   bot.user.setGame(`Fallout 4`)
